9a73c5d3c29bfb8167b72640203fcb77eb180ce4
longze chen <cs.longze.chen@gmail.com>
2017-03-22 15:25:58 -0400

    Update postgres for ssh/rsync as root.

--- postgres/postgres-9.6/docker-run.sh
+++ postgres/postgres-9.6/docker-run.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-IMAGE_NAME="postgres-9.6"
-TAG="0.10"
+if [ $# == 1 ] && [ $1 == "-d" ]; then
+    DAEMON="-d"
+else
+    DAEMON=""
+fi
 
-HOME="/home/"
+IMAGE_NAME="postgres-9.6"
+TAG="0.20"
+
+HOME="/root/"
 POSTGRES_DATA="/var/lib/postgresql/data/"
 
-PORT_MAPPING="192.168.168.167:6543:5432"
+PORT_POSTGRES="192.168.168.167:6432:5432"
+PORT_SSH="192.168.168.167:6022:22"
 
-docker run -d -p $PORT_MAPPING -v $PWD/data/pgdata:$POSTGRES_DATA -v $PWD/data/home:$HOME $IMAGE_NAME:$TAG
+
+docker run $DAEMON -p $PORT_POSTGRES -p $PORT_SSH -v $PWD/data/pgdata:$POSTGRES_DATA -v $PWD/data/home:$HOME $IMAGE_NAME:$TAG
 
