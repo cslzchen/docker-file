ca1521d30b107a7896751f7692934efc4d1a5e54
longze chen <cs.longze.chen@gmail.com>
2017-03-22 15:17:40 -0400

    Update barman for ssh/rsync as root.

--- barman/barman-2.1/docker-run.sh
+++ barman/barman-2.1/docker-run.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-IMAGE_NAME="barman-2.1"
-TAG="0.10"
+if [ $# == 1 ] && [ $1 == "-d" ]; then
+    DAEMON="-d"
+else
+    DAEMON=""
+fi
 
-HOME="/home/"
+IMAGE_NAME="barman-2.1"
+TAG="0.20"
+
+HOME="/root/"
 BARMAN_CONFIG="/etc/barman.d/"
+BARMAN_LIB="/var/lib/barman/"
 POSTGRES_DATA="/var/lib/postgresql/data/"
 
-PORT_MAPPING="192.168.168.167:7654:5432"
+PORT_POSTGRES="192.168.168.167:7432:5432"
+PORT_SSH="192.168.168.167:7022:22"
 
-docker run -d -p $PORT_MAPPING -v $PWD/data/barman.d:$BARMAN_CONFIG -v $PWD/data/pgdata:$POSTGRES_DATA -v $PWD/data/home:$HOME $IMAGE_NAME:$TAG
+docker run $DAEMON -p $PORT_POSTGRES -p $PORT_SSH -v $PWD/data/barman_lib:$BARMAN_LIB -v  $PWD/data/barman.d:$BARMAN_CONFIG -v $PWD/data/pgdata:$POSTGRES_DATA -v $PWD/data/home:$HOME $IMAGE_NAME:$TAG
 
